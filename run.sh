3873c48cbe8fb292eae5f00f6de8f7ff099bc54c
Dan Georges Berrebbi <dberrebb@tir.lti.cs.cmu.edu>
2021-11-20 13:26:01 -0500

    run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+python training/train.py \
+--data "training/output.dan" \
+--logging_steps 50 \
+--max_steps 6000 \
+--evaluation_strategy steps \
+--save_strategy steps \
+--eval_steps 500 \
+--save_steps 500 \
+--learning_rate 0.0004 \
+--adam_beta2 0.98 \
+--adam_epsilon 1e-06 \
+--dropout 0.1 \
+--weight_decay 0.01  \
+--output_dir ~/runs/pretrained_shiba \
+--masking_type rand_span \
+--gradient_accumulation_steps 6 \
+--masking_type rand_span \
+--deep_transformer_stack_layers 12 \
+--per_device_eval_batch_size 2 \
+--per_device_train_batch_size 4
