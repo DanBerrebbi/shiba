dac2666c847734d6709c321ba5814c73a0127ff6
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2021-11-20 17:02:37 -0500

    cgt vocab bug

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+masking=bpe_span  # masking is rand_span or bpe_span or rand_char
+echo $masking
+
 python training/train.py \
---data "training/output.dan" \
+--data "training/all_examples.jsonl" \
 --logging_steps 50 \
 --max_steps 20000 \
 --evaluation_strategy steps \
@@ -13,11 +16,11 @@ python training/train.py \
 --adam_epsilon 1e-06 \
 --dropout 0.1 \
 --weight_decay 0.01  \
---output_dir "training/models/first" \
+--output_dir "training/models/first_${masking}" \
 --overwrite_output_dir True \
 --masking_type rand_span \
 --gradient_accumulation_steps 6 \
---masking_type rand_span \
+--masking_type $masking \
 --deep_transformer_stack_layers 12 \
 --per_device_eval_batch_size 12 \
 --per_device_train_batch_size 16
