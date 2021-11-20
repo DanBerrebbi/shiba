a18b3816846335ee7cd1c0c305cf695a84d6c712
Dan Georges Berrebbi <dberrebb@tir-0-3.eth>
2021-11-20 15:59:38 -0500

    binbash

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 python training/train.py \
 --data "training/output.dan" \
 --logging_steps 50 \
@@ -17,5 +19,5 @@ python training/train.py \
 --gradient_accumulation_steps 6 \
 --masking_type rand_span \
 --deep_transformer_stack_layers 12 \
---per_device_eval_batch_size 4 \
---per_device_train_batch_size 4
+--per_device_eval_batch_size 12 \
+--per_device_train_batch_size 12
