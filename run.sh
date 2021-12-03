1b004fd80e472d07e890a17be09ae9a51e2a2c92
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2021-12-02 20:55:21 -0500

    expalancer

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ python training/train.py \
 --adam_epsilon 1e-06 \
 --dropout 0.1 \
 --weight_decay 0.01  \
---output_dir "training/models/contrastive_${masking}" \
+--output_dir "training/models/exp_contrast_alpha0.5" \
 --overwrite_output_dir False \
 --masking_type rand_span \
 --gradient_accumulation_steps 6 \
