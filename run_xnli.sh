bb4ca8e27876b0f89692eb651d3cddd79f704d47
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2021-12-04 12:13:40 -0500

    debug xnli

--- run_xnli.sh
+++ run_xnli.sh
@@ -13,4 +13,4 @@ python3 train_xnli.py \
 --adam_beta2 0.98 \
 --adam_epsilon 1e-06 \
 --dropout 0.1 \
---resume_from_checkpoint "/projects/tir5/users/dberrebb/CANINE/forked_shiba/shiba/training/models/${model}/checkpoint-10000/pytorch_model.bin"
+#--resume_from_checkpoint "/projects/tir5/users/dberrebb/CANINE/forked_shiba/shiba/training/models/${model}/checkpoint-10000/pytorch_model.bin"
