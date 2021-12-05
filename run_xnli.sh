1bf8f11baded6f1672eb8e8107d49dba53542834
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2021-12-05 10:36:38 -0500

    alala

--- run_xnli.sh
+++ run_xnli.sh
@@ -3,6 +3,7 @@
 model="exp3"
 python3 train_xnli.py \
 --output_dir "XNLI_MODELS/${model}" \
+--num_train_epochs 10
 --logging_steps 50 \
 --max_steps 5000 \
 --evaluation_strategy steps \
@@ -13,5 +14,12 @@ python3 train_xnli.py \
 --adam_beta2 0.98 \
 --adam_epsilon 1e-06 \
 --dropout 0.1 \
+--overwrite_output_dir False \
+--masking_type rand_span \
+--gradient_accumulation_steps 24 \
+--masking_type $masking \
+--deep_transformer_stack_layers 12 \
+--per_device_eval_batch_size 2 \
+--per_device_train_batch_size 4 \
 --resume_from_checkpoint "/projects/tir5/users/dberrebb/CANINE/forked_shiba/shiba/training/models/${model}/checkpoint-10000/pytorch_model.bin"
 
