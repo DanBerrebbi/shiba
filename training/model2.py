ee668f978c38dde107a05d831cdbbd82ea2ecb73
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2021-12-04 19:50:24 -0500

    alala

--- training/model2.py
+++ training/model2.py
@@ -347,7 +347,8 @@ class ShibaForClassification(ShibaForTask):
                                               predict_indices=None)['embeddings'][:, 0, :]
 
         cls_embeddings = self.label_layer2(cls_embeddings)
-        class_hidden_states = self.label_layer(torch.nn.ReLU(cls_embeddings))
+        cls_embeddings=torch.nn.ReLU(cls_embeddings)
+        class_hidden_states = self.label_layer(cls_embeddings)
         #class_probs = self.log_softmax(class_hidden_states)
         class_probs = torch.nn.functional.log_softmax(class_hidden_states, dim=-1)  # tester ca mais je crois que log_spftmax marche mieux de maniere generale
 
