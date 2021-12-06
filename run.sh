b376416d52849b44f02f58f363e468234c5123ad
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2021-12-06 01:04:58 -0500

    db6

--- run.sh
+++ run.sh
@@ -16,13 +16,13 @@ python training/train.py \
 --adam_epsilon 1e-06 \
 --dropout 0.1 \
 --weight_decay 0.01  \
---output_dir "training/models/exp_simclr_alpha0.5_bs6_acc15_ngpu4" \
+--output_dir "training/models/exp_simclr_alpha0.5_bs6_acc15_ngpu4_OVER_JIMIN" \
 --overwrite_output_dir False \
 --masking_type rand_span \
---gradient_accumulation_steps 15 \
+--gradient_accumulation_steps 20 \
 --masking_type $masking \
 --deep_transformer_stack_layers 12 \
 --per_device_eval_batch_size 2 \
---per_device_train_batch_size 6 \
-#--resume_from_checkpoint "/projects/tir5/users/dberrebb/CANINE/forked_shiba/shiba/training/models/first_rand_span/checkpoint-1500" \
+--per_device_train_batch_size 4 \
+--resume_from_checkpoint "/projects/tir5/users/dberrebb/CANINE/forked_shiba/shiba/training/models/jimin_rand_span/pytorch_model.bin" \
 
