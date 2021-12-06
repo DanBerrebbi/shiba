58c17b63b07e5d3bc60c09ae2aa3cfcd4ac056ea
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2021-12-06 01:10:04 -0500

    db6

--- run.sh
+++ run.sh
@@ -19,9 +19,9 @@ python training/train.py \
 --output_dir "training/models/exp_simclr_alpha0.5_bs6_acc15_ngpu4" \
 --overwrite_output_dir False \
 --masking_type rand_span \
---gradient_accumulation_steps 20 \
+--gradient_accumulation_steps 60 \
 --masking_type $masking \
---deep_transformer_stack_layers 12 \
+--deep_transformer_stack_layers 6 \
 --per_device_eval_batch_size 2 \
 --per_device_train_batch_size 4 \
 #--resume_from_checkpoint "/projects/tir5/users/dberrebb/CANINE/forked_shiba/shiba/training/models/jimin_rand_span/pytorch_model.bin" \
